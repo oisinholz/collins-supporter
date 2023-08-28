7a18931c70be837cee389e0fd3e012fcdea89a65
oisinholz <oisinholzinger@outlook.com>
2023-08-28 14:16:03 +0100

    small fixes - start on email fucntionality

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,7 @@ import useFetch from "./useFetch";
 import { useState } from "react";
 
 import { useNavigate } from 'react-router-dom';
+// import nodemailer from "nodemailer";
 
 // const nodemailer = require("nodemailer");
 
@@ -101,41 +102,6 @@ const FormDetails = () => {
 
         console.log(form.title + ' submitted: ');
         console.log(submitedForm);
-
-        //     try {
-        //         let transporter = nodemailer.createTransport({
-        //             host: "mailhub.utc.com",
-        //             port: 25,
-        //             secure: true,
-        //             auth: {
-        //                 user: "your-email@gmail.com",
-        //                 pass: "your-password",
-        //             },
-        //         });
-
-        //         transporter.sendMail({
-        //             from: '"CollinsAidTool" <your-email@gmail.com>',
-        //             to: "shannonIT@utas.utc.com",
-        //             subject: "Form Submission Details",
-        //             html: `
-        //     <h1>Form Submission Details</h1>
-        //     <h2>This is a test</h2>
-        //     <p>Form ID: ${id}</p>
-        //     <p>Option A: ${optionA}</p>
-        //     <p>Other A: ${otherA}</p>
-        //     <p>Option B: ${optionB}</p>
-        //     <p>Other B: ${otherB}</p>
-        //     <p>Option A: ${desc}</p>
-        //     <p>Option B: ${name}</p>    
-        //   `,
-        //         });
-
-        //         console.log("Email sent");
-        //     } catch (error) {
-        //         console.error("Error sending email: " + error.message);
-        //     }
-
-
 
         const parseID = parseInt(id);
 
